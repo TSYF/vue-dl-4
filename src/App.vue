421877a0ef797f0711b72f6a612cff0c4e63824d
TSYF <tsyf1999@gmail.com>
2022-03-13 23:01:23 -0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="form">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <h1>Lista de tareas</h1>
+        <form @submit.prevent="submit()">
+            <input
+                type="text"
+                placeholder="Ingrese tareas para hacer"
+                v-model="input"
+            />
+            <button type="submit">Añadir</button>
+        </form>
+        <hr />
+
+        <form-list :tareas="tareas" @eliminar-tarea="eliminarTarea" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import FormList from "./components/FormList.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    components: { FormList },
+    data() {
+        return {
+            input: undefined,
+        };
+    },
+    methods: {
+        submit() {
+            this.tareas.push(this.input);
+            return (this.input = undefined);
+        },
+        eliminarTarea(val) {
+            this.tareas = this.tareas.filter((item, index) => index !== val);
+        }
+    },
+    computed: {
+      tareas() {
+        return []
+      }
+    },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    ul {
+        padding: 0;
+    }
 }
 </style>
